2677a0be13822ac728272a397adbe772496370aa
Jay Castro <33559928+JayCastro97@users.noreply.github.com>
2020-11-18 19:01:42 -0600

    update adder_subtractor to 32-bit + readME

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Kolache-ALU
 32-bit ALU written in iVerilog
+
+###How to compile testbench:
+**iverilog -o tb.vvp adder_subtractor_testbench.v**
+
+### How to compile component: 
+iverilog -o <filename2>.vvp <filename1>.v
+### How to run component:
+vvp <filename2>.vvp
+
--- testbench/adder_subtractor_testbench.v
+++ testbench/adder_subtractor_testbench.v
@@ -1,27 +1,26 @@
 `timescale 1ns/1ns
-`include "../16_bit_adder_subtractor.v"
+`include "../32_bit_adder_subtractor.v"
 
 module adder_subtractor_testbench;
-reg [15:0] a, b;
+reg [31:0] a, b;
 reg M;
-wire [15:0] sum;
+wire [31:0] sum;
 wire c_out;
 integer i,j;
 Adder_subtractor dut (.a(a), .b(b), .M(M), .c_out(c_out), .sum(sum));
-//Mul4 dut1 (.a(a), .b(b), .M(M), .c_out(c_out), .sum(sum));
 initial begin
-$monitor("a = %b b = %b M = %b c_out = %b sum = %b", a, b, M, c_out, sum);
-M = 1'b0;
-for (i = 5; i < 8 ; i = i + 1) begin
-for (j = 6; j < 9 ; j = j + 1) begin
-a = i; b = j; #20;
+$monitor("a = %b b = %b M = %b c_out = %b sum = %b", a, b, M, c_out, sum); 
+M = 1'b0;  // ADDER
+for (i = 5; i < 8 ; i = i + 1) begin // start from a = 5 
+    for (j = 6; j < 9 ; j = j + 1) begin // start from b = 6
+        a = i; b = j; #20;
+    end
 end
-end
-M = 1'b1;
+M = 1'b1; // SUBTRACTOR
 for (i = 6; i < 8 ; i = i + 1) begin
-for (j = 0; j < 5 ; j = j + 1) begin
-a = i; b = j; #20;
-end
+    for (j = 0; j < 5 ; j = j + 1) begin
+        a = i; b = j; #20;
+    end
 end
 end
 endmodule
