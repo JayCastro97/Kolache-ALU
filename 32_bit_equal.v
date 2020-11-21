6124902fc6be42f40a08574c6e0865ab5e4239c1
Jay Castro <33559928+JayCastro97@users.noreply.github.com>
2020-11-20 20:40:18 -0600

    create 32_input AND gate

--- 32_bit_equal.v
+++ 32_bit_equal.v
@@ -1,3 +1,4 @@
+`include "../32x1_and.v"
 module EQ (
 input a, b,
 output y
@@ -7,15 +8,21 @@ xor(n,a,b);
 not(y,n);
 
 endmodule
-
+//temporary 2-bit equal
 module equal (
-    input [31:0] a, b, output y
-)
+    input [1:0] a, b, output y
+);
 // initialize 32_bit AND gate
+wire c1,c2;
+// initialize two EQ components 
+EQ m1(.a(a[1]), .b(b[1]),.y(c1));
+EQ m2(.a(a[0]), .b(b[0]),.y(c2));
+// initialize 
+and(y,c1,c2);
+//and_4x1 u1(.a(a[3:2]),.b(b[3:2]), .y(y));
 
-wire c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15,
- c16,c17, c18, c19, c20, c21, c22, c23, c24, c25, c26, c27, c28, c29, c30, c31;
 
 
-
+//wire c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15,
+// c16,c17, c18, c19, c20, c21, c22, c23, c24, c25, c26, c27, c28, c29, c30, c31;
 endmodule 
